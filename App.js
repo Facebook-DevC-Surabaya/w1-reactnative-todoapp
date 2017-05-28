20bb294e5c4536a07505c755747fcfacd12db970
Firdaus <firdausious@gmail.com>
2017-05-28 12:09:48 +0700

    state 1

--- App.js
+++ App.js
@@ -1,14 +1,106 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  StyleSheet,
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  View
+} from 'react-native';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    // deklarasi state
+    this.state = {
+      text: '',
+      todos: []
+    };
+  }
+
+  // Tambah todo
+  add = () => {
+    // Dapatkan value dari state sekarang
+    const { text, todos } = this.state;
+
+    // Jika text kosong, skip
+    if (!text) return;
+
+    // Masukkan todo baru ke array
+    todos.push(text);
+
+    // Set state baru
+    this.setState({
+      todos: todos,
+      text: ''
+    });
+  }
+
+  // Hapus todo
+  remove = (todo) => {
+    const { todos } = this.state;
+
+    // Cari index dari todo
+    todoWillDelete = todos.indexOf(todo);
+
+    // Hapus todo
+    todos.splice(todoWillDelete, 1);
+
+    // Set state baru
+    this.setState({
+      todos: todos,
+    });
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={styles.spacer} />
+
+          <Text>Simple Todo App</Text>
+
+          <View style={styles.spacer} />
+
+          {/* Form input todo */}
+          <View>
+            <TextInput
+              style={styles.textInput}
+              onChangeText={(text) => this.setState({text})}
+              value={this.state.text}
+            />
+
+            <Button
+              title="Save"
+              color="#ff0000"
+              onPress={this.add}
+            />
+          </View>
+
+          <View style={styles.spacer} />
+
+          {/* Tampilan todos jika ada */}
+          {this.state.todos.length > 0 &&
+            <View>
+              {
+                this.state.todos.map((todo, idx) =>
+                  <TouchableHighlight
+                    key={idx}
+                    onPress={() => this.remove(todo)}
+                  >
+                    <Text style={styles.todo}>
+                      - {todo}
+                    </Text>
+                  </TouchableHighlight>
+                )
+              }
+            </View>
+          }
+        </View>
+      </ScrollView>
     );
   }
 }
@@ -17,7 +109,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20
   },
+  button: {},
+  textInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    padding: 10
+  },
+  spacer: {
+    left: 0,
+    right: 0,
+    height: 30,
+  },
+  todo: {
+    marginBottom: 20
+  }
 });
